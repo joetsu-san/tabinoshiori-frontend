167643a89d1d1a1cadb9062399d2d58b21d380d4
mitsuYashi <hm20011027@gmail.com>
2023-06-26 16:53:02 +0900

    change: routing

--- src/app/travelplan/page.tsx
+++ src/app/travelplan/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import type { NextPage } from "next";
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Group, Button } from "@mantine/core";
+import { Modal, Group, Button, Textarea, Text } from "@mantine/core";
 import { IconShare } from "@tabler/icons-react";
-import { TimeLineWrapper } from "./_components/TimeLineWrapper";
-import { ShareModalContent } from "./_components/ShareModalContent";
+// import { TimeLineWrapper } from "./_components/TimeLineWrapper";
+import { ShareModalContent } from "./[id]/_components/ShareModalContent";
 
 const Page: NextPage = () => {
-  const [opened, { open, close }] = useDisclosure(false);
   return (
     <main
       style={{
@@ -16,22 +15,7 @@ const Page: NextPage = () => {
         alignItems: "center",
         minHeight: "100vh",
       }}
-    >
-      <Modal opened={opened} onClose={close} title="旅のしおりを共有する" centered>
-        <ShareModalContent />
-      </Modal>
-      <Group position="center" m={10}>
-        <Button color="cyan" variant="light" compact onClick={open} leftIcon={<IconShare size="1rem" />}>
-          旅のしおりを共有する
-        </Button>
-      </Group>
-
-      <h1>旅のしおり</h1>
-      <h2>上越旅行</h2>
-      <textarea />
-      <TimeLineWrapper />
-      {/* <EditorContent style={{ width: "100%", height: "100%", border: "1px solid #000" }} editor={editor} /> */}
-    </main>
+    ></main>
   );
 };
 

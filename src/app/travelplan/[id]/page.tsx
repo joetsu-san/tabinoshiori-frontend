0227b7c6905de36a09412dc1649c528dd407cfe1
mitsuYashi <hm20011027@gmail.com>
2023-06-27 17:21:35 +0900

    feat: travelplan element

--- src/app/travelplan/[id]/page.tsx
+++ src/app/travelplan/[id]/page.tsx
@@ -1,12 +1,23 @@
 "use client";
-import { Modal, Group, Button, Textarea, Text } from "@mantine/core";
-import { IconShare } from "@tabler/icons-react";
+import { Modal, Group, Button, Textarea, Text, Flex, Input, TextInput } from "@mantine/core";
+import { IconDeviceMobileMessage, IconPictureInPicture, IconScreenshot, IconShare } from "@tabler/icons-react";
 import { TimeLineWrapper } from "./_components/TimeLineWrapper";
 import { ShareModalContent } from "./_components/ShareModalContent";
 import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 const Page = () => {
   const [opened, { open, close }] = useDisclosure(false);
+
+  const [title, setTitle] = useState("タイトル");
+  const [description, setDescription] = useState("説明");
+  const [isTitleInput, setIsTitleInput] = useState(false);
+  const [isDescriptionInput, setIsDescriptionInput] = useState(false);
+  const openTitleInput = () => setIsTitleInput(true);
+  const closeTitleInput = () => setIsTitleInput(false);
+  const openDescriptionInput = () => setIsDescriptionInput(true);
+  const closeDescriptionInput = () => setIsDescriptionInput(false);
+
   return (
     <main
       style={{
@@ -23,13 +34,50 @@ const Page = () => {
         <Button color="cyan" variant="light" compact onClick={open} leftIcon={<IconShare size="1rem" />}>
           旅のしおりを共有する
         </Button>
+        <Button color="cyan" variant="light" compact leftIcon={<IconScreenshot size="1rem" />}>
+          画像化する
+        </Button>
       </Group>
-      <Text>上越旅行</Text>
-      <Textarea />
+
+      <Flex direction="column">
+        <Flex justify="space-between">
+          {isTitleInput ? (
+            <>
+              <Input defaultValue={title} />
+              <Button color="gray" variant="light" compact onClick={closeTitleInput}>
+                終了
+              </Button>
+            </>
+          ) : (
+            <>
+              <Text fz="xl">{title}</Text>
+              <Button color="gray" variant="light" compact onClick={openTitleInput}>
+                編集
+              </Button>
+            </>
+          )}
+        </Flex>
+
+        <Flex justify="space-between">
+          {isDescriptionInput ? (
+            <>
+              <TextInput defaultValue={description} onChange={(event) => setTitle(event.currentTarget.value)} />
+              <Button color="gray" variant="light" compact onClick={closeDescriptionInput}>
+                終了
+              </Button>
+            </>
+          ) : (
+            <>
+              <Text>{description}</Text>
+              <Button color="gray" variant="light" compact onClick={openDescriptionInput}>
+                編集
+              </Button>
+            </>
+          )}
+        </Flex>
+      </Flex>
       <TimeLineWrapper />
     </main>
-
-    //   <TimeLineWrapper />
   );
 };
 

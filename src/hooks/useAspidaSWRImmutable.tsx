dec4af4b3d3e25dd00108988ca67a53ed5033b31
mitsuYashi <hm20011027@gmail.com>
2023-06-27 17:20:26 +0900

    change: file name

--- src/hooks/useAspidaSWRImmutable.tsx
+++ src/hooks/useAspidaSWRImmutable.tsx
@@ -4,7 +4,7 @@ import useAspidaSWR from "@aspida/swr";
 
 export const client = api(aspida(fetch, { baseURL: process.env.BASE_URL }));
 
-export const useAspidaSWRImutable = (path: any, query: any) => {
+export const useAspidaSWRImmutable = (path: any, query: any) => {
   const { data, error } = useAspidaSWR(path, query, {
     revalidateIfStale: false,
     revalidateOnFocus: false,

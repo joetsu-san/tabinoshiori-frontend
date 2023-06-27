dc6875a4197083fbfcfb9cb9c5ffa649a81663af
mitsuYashi <hm20011027@gmail.com>
2023-06-27 12:19:34 +0900

    change: baseURL

--- src/hooks/useAspidaSWRMutate.tsx
+++ src/hooks/useAspidaSWRMutate.tsx
@@ -2,7 +2,7 @@ import api from "../../api/$api";
 import aspida from "@aspida/fetch";
 import useAspidaSWR from "@aspida/swr";
 
-export const client = api(aspida());
+export const client = api(aspida(fetch, { baseURL: process.env.BASE_URL }));
 
 export const useAspidaSWRImutable = (path: any, query: any) => {
   const { data, error } = useAspidaSWR(path, query, {

f8d445070c78d89aef8125f9def49b740c1c1488
mitsuYashi <hm20011027@gmail.com>
2023-07-03 18:18:53 +0900

    change: request

--- src/lib/createUser.tsx
+++ src/lib/createUser.tsx
@@ -2,12 +2,10 @@ import { client } from "@/lib/aspida";
 
 export const createUser = async (token: string, name: string) => {
   const user = await client.user.login.$post({
-    config: { headers: { Authorization: `Bearer ${token}` } },
     body: {
       name: name,
-      birthday: "0000-00-00",
-      genderId: 1,
     },
+    config: { headers: { Authorization: `Bearer ${token}` } },
   });
 
   return user;

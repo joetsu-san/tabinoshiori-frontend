329224055aba2ede2fc926ef740ee88e1f3dd47f
mitsuYashi <hm20011027@gmail.com>
2023-07-03 17:10:15 +0900

    feat: axios config

--- src/lib/aspida.ts
+++ src/lib/aspida.ts
@@ -1,5 +1,8 @@
+import axios from "axios";
 import api from "../../api/$api";
 import aspida from "@aspida/fetch";
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const fetchConfig = {
   baseURL: process.env.NEXT_PUBLIC_API_URL,

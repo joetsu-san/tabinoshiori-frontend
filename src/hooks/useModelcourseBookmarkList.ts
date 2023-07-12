1f0277094e3d57b15167c95890e92db345eb8bc0
mitsuYashi <hm20011027@gmail.com>
2023-07-13 03:20:39 +0900

    change: mutate

--- src/hooks/useModelcourseBookmarkList.ts
+++ src/hooks/useModelcourseBookmarkList.ts
@@ -5,12 +5,10 @@ import { useRecoilValue } from "recoil";
 
 export const useModelcourseBookmarkList = () => {
   const token = useRecoilValue(firebaseTokenState);
-  const { data, error } = useAspidaSWR(client.user.model_course_bookmark, "$get", {
+  return useAspidaSWR(client.user.model_course_bookmark, "$get", {
     config: {
       headers: { Authorization: `Bearer ${token}` },
     },
     enabled: !!token,
   });
-
-  return { data, error };
 };

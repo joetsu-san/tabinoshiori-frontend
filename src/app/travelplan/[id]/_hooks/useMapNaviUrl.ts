63fffcde4beeed425d0898f8ad1b3a405ae04995
mitsuYashi <hm20011027@gmail.com>
2023-07-23 15:26:43 +0000

    refactor: delete useEffect

--- src/app/travelplan/[id]/_hooks/useMapNaviUrl.ts
+++ src/app/travelplan/[id]/_hooks/useMapNaviUrl.ts
@@ -12,35 +12,26 @@ const generateMapNaviUrlOfLast = (latitude: number, longitude: number) => {
 };
 
 export const useMapNaviUrl = (travelPlanId: string) => {
-  const travelPlan = useTravelPlan(travelPlanId);
-  const [mapNaviUrl, setMapNaviUrl] = useState<string>("");
   const baseUrl = "https://www.google.com/maps/dir/?api=1";
+  const travelPlan = useTravelPlan(travelPlanId);
+  let mapNaviUrl = baseUrl;
+  let waypoints = `&waypoints=`;
 
-  const generateMapNaviUrls = useCallback(() => {
-    let url = baseUrl;
-    let waypoints = `&waypoints=`;
-    if (!travelPlan) return;
+  if (!travelPlan) return { error: "旅程が見つかりませんでした。" };
 
-    for (const [index, spot] of travelPlan.travelPlanSpots.entries()) {
-      if (index === 0) {
-        url += generateMapNaviUrlOfFirst(spot.latitude, spot.longitude);
-        continue;
-      }
-      if (index === travelPlan.travelPlanSpots.length - 1) {
-        url += generateMapNaviUrlOfLast(spot.latitude, spot.longitude);
-        continue;
-      }
-      if (index !== 1) waypoints += "|";
-      waypoints += `${spot.latitude},${spot.longitude}`;
+  for (const [index, spot] of travelPlan.travelPlanSpots.entries()) {
+    if (index === 0) {
+      mapNaviUrl += generateMapNaviUrlOfFirst(spot.latitude, spot.longitude);
+      continue;
     }
-    url += waypoints;
-
-    setMapNaviUrl(url);
-  }, [travelPlan]);
-
-  useEffect(() => {
-    generateMapNaviUrls();
-  }, [travelPlan]);
+    if (index === travelPlan.travelPlanSpots.length - 1) {
+      mapNaviUrl += generateMapNaviUrlOfLast(spot.latitude, spot.longitude);
+      continue;
+    }
+    if (index !== 1) waypoints += "|";
+    waypoints += `${spot.latitude},${spot.longitude}`;
+  }
+  mapNaviUrl += waypoints;
 
   return { mapNaviUrl };
 };
